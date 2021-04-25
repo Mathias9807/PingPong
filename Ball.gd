877512a28cd08d8df52c23c299810e5259267624
Mathias Johansson <mathias9807@gmail.com>
2021-04-25 22:02:45 +0200

    Start paddle physics

--- Ball.gd
+++ Ball.gd
@@ -1,12 +1,40 @@
-extends RigidBody
+extends KinematicBody
+
+export (Vector3) var startVel : Vector3 = Vector3()
 
 var marker
 var rayCastResult
+var oldState
+
+var vel
 
 func _ready():
 	marker = get_node("Marker")
+	vel = startVel
 
 func _physics_process(delta):
+	var physicsSpeed = 1
+	var gravity = 5
+	var bounce = 1
+	vel += Vector3(0, -1, 0) * gravity * delta * physicsSpeed
+	
+	var move = vel * delta
+	
+	while move.length() > 0.0001:
+		var collision = move_and_collide(vel * delta * physicsSpeed)
+		if collision:
+			# Built in bounce way inconsistent. We'll have to do it ourselves.
+			# Find ball speed relative to object and with the impact points normal
+			# axis. Add 2*s*bounce to speed
+			# TODO: Implement tourque
+			var rVel = vel - collision.collider_velocity
+			var rSpeed = rVel.dot(collision.normal)
+			vel -= 2 * rSpeed * bounce * collision.normal
+			
+			move = collision.remainder
+		else:
+			break
+	
 	var space_state = get_world().direct_space_state
 	var pos = self.translation + Vector3(0, 0.02, 0)
 	rayCastResult = space_state.intersect_ray(pos, Vector3(pos.x, -1, pos.z), [self])
@@ -19,6 +47,16 @@ func _process(delta):
 	else:
 		marker.visible = false
 
+#func _integrate_forces(state):
+#	for i in range(state.get_contact_count()):
+#		print(state.get_contact_local_normal(i))
+#		print(state.get_contact_impulse(i))
+#		print(state.)
+#		# var imp = state.get_contact_impulse(i)
+#		# var adj = 0.01
+#		# state.apply_central_impulse(state.get_contact_local_normal(i) * adj)
+#	
+#	oldState = state
 
 func _on_Ball_body_shape_entered(body_id, body, body_shape, local_shape):
-	print(body)
+	pass
--- Paddle.gd
+++ Paddle.gd
@@ -1,33 +1,38 @@
 extends KinematicBody
 
 var target
-var distFromTarget = 1.5
+export (float) var distFromTarget : float = 1.5
 
-var maxReachAng = Vector2(1.0, 0.5)
+var maxReachAng = Vector2(0.7, 0.2)
 var basePos
+var baseRot = Vector2(0, 0)
 var baseAxis = Vector3(1, 0, 0)
-
-var time = 0
+var vertAxis = Vector3(0, 1, 0)
+var sideAxis = vertAxis.cross(baseAxis)
 
 func _ready():
 	target = transform.origin
 
 func _physics_process(delta):
-	var vel = Vector3(0, 0.2 * cos(time * 30), 0)
-	time += delta
+	var vel = Vector3(0, 0, 0)
 	
 	var collision_info = move_and_collide(vel * delta)
 	if collision_info:
 		var collision_point = collision_info.position
 
 func _process(delta):
-	pass
+	transform = Transform()
+	var leftJoy = Vector2(Input.get_joy_axis(0, JOY_AXIS_0), Input.get_joy_axis(0, JOY_AXIS_1))
+	basePos = baseToGlobal(leftJoy)
+	baseRot = Vector2(leftJoy[0] * maxReachAng[0], leftJoy[1] * maxReachAng[1])
+	transform = transform.rotated(vertAxis, baseRot.x)
+	transform = transform.rotated(sideAxis, baseRot.y + PI/2)
+	transform.origin = basePos
 
 # Convert left-joystick coordinates (-1:1) to global position
 func baseToGlobal(coords):
 	var pos = baseAxis * distFromTarget
-	var vertAxis = Vector3(0, 1, 0)
-	pos.rotated(vertAxis, coords[0] * maxReachAng[0])
-	pos.rotated(vertAxis.cross(baseAxis), coords[1] * maxReachAng[1])
+	pos = pos.rotated(vertAxis, coords[0] * maxReachAng[0])
+	pos = pos.rotated(sideAxis, coords[1] * maxReachAng[1])
 	
 	return target + pos
