856ae1b6cd00744c060123482ba0f1f06cfa068c
Mathias Johansson <mathias9807@gmail.com>
2021-04-25 22:26:31 +0200

    Paddle depth movement with right stick

--- Paddle.gd
+++ Paddle.gd
@@ -6,32 +6,36 @@ export (float) var distFromTarget : float = 1.5
 var maxReachAng = Vector2(0.7, 0.2)
 var basePos
 var baseRot = Vector2(0, 0)
+var targetTransform = Transform()
 var baseAxis = Vector3(1, 0, 0)
 var vertAxis = Vector3(0, 1, 0)
 var sideAxis = vertAxis.cross(baseAxis)
 
 func _ready():
 	target = transform.origin
+	basePos = baseToGlobal(Vector2())
 
 func _physics_process(delta):
-	var vel = Vector3(0, 0, 0)
-	
-	var collision_info = move_and_collide(vel * delta)
-	if collision_info:
-		var collision_point = collision_info.position
+	var transSpeed = 12
+	transform = transform.interpolate_with(targetTransform, transSpeed * delta)
 
 func _process(delta):
-	transform = Transform()
 	var leftJoy = Vector2(Input.get_joy_axis(0, JOY_AXIS_0), Input.get_joy_axis(0, JOY_AXIS_1))
-	basePos = baseToGlobal(leftJoy)
-	baseRot = Vector2(leftJoy[0] * maxReachAng[0], leftJoy[1] * maxReachAng[1])
-	transform = transform.rotated(vertAxis, baseRot.x)
-	transform = transform.rotated(sideAxis, baseRot.y + PI/2)
-	transform.origin = basePos
+	var rightJoy = Vector2(Input.get_joy_axis(0, JOY_AXIS_2), Input.get_joy_axis(0, JOY_AXIS_3))
+	
+	var zVel = rightJoy.y * 0.1
+	
+	basePos = baseToGlobal(leftJoy, distFromTarget + zVel)
+	baseRot = Vector2(leftJoy[0] * maxReachAng[0],leftJoy[1] * maxReachAng[1])
+	
+	targetTransform = Transform()
+	targetTransform = targetTransform.rotated(vertAxis, 2 * baseRot.x)
+	targetTransform = targetTransform.rotated(sideAxis, 2 * baseRot.y + PI/2)
+	targetTransform.origin = basePos
 
 # Convert left-joystick coordinates (-1:1) to global position
-func baseToGlobal(coords):
-	var pos = baseAxis * distFromTarget
+func baseToGlobal(coords, radius=distFromTarget):
+	var pos = baseAxis * radius
 	pos = pos.rotated(vertAxis, coords[0] * maxReachAng[0])
 	pos = pos.rotated(sideAxis, coords[1] * maxReachAng[1])
 	
